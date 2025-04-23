b04a30a7a37c02bca65b624f3523c151935b75a3
Wagner <wagnerpalmeira.ti@gmail.com>
2025-04-23 16:10:58 -0300

    Adicionando exemplo para alunos

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .hello import hello_world
+from .calculadora import soma, subtracao, multiplicacao
---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def soma(x, y):
+    """Soma dois números."""
+    return x + y
+
+def subtracao(x, y):
+    """Subtrai dois números."""
+    return x - y
+
+def multiplicacao(x, y):
+    """Multiplica dois números."""
+    return x * y
